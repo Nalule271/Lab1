5bd7272de57f8ef1eee752bf041cd74cdc70d7e6
Nalule271 <nalulem25@mru.ac.ug>
2026-09-02 10:09:19 +0300

    Add files via upload
    Lab1.java

---
+++ Lab1.java
@@ -0,0 +1,28 @@
+public class Lab1{
+public static void main(String[]args)
+{
+    // University Student;
+    int Age =20;
+char Gender ='M';
+String Name = "MERCY RHINA";
+double CGPA = 4.20;
+boolean Paid_Tution = true;
+float Weight = 50.2f;
+long Contact = 709355929L;
+byte Retakecounts = 2;
+short Roomnumber = 214;
+
+System.out.println("=======Student's Profile========");
+System.out.println("Age:"+Age);
+System.out.println("Name:"+Name);
+System.out.println("Gender:"+Gender);
+System.out.println("CGPA:"+CGPA);
+System.out.println("Paid_Tution:"+Paid_Tution);
+System.out.println("Weight:"+Weight);
+System.out.println("Contact:"+ Contact);
+System.out.println("Retakecounts:"+Retakecounts);
+System.out.println("Roomnumber:"+Roomnumber);
+}
+
+}
+
