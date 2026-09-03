1f60eba72e21bfea6b98ef7551bb26c3f434cf36
Nalule271 <nalulem25@mru.ac.ug>
2026-09-03 15:08:55 +0300

    Add files via upload
    Bug explanation:
    
    The bug is that bonus Marks was declared without being initialized before it was used

---
+++ SnippetB.java
@@ -0,0 +1,11 @@
+public class SnippetB {
+    public static void main(String[] args) {
+
+        int bonusMarks = 5;
+        double examMark = 68.0;
+
+        double finalMark = examMark + bonusMarks;
+
+        System.out.println("Final mark: " + finalMark);
+    }
+}
